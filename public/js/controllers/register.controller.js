022d5434ac4eadcd6d2b0813cb0cd74310fc7d3e
bradwat <bradwat@live.com>
2016-04-01 19:47:18 -0400

    finished authservice

--- public/js/controllers/register.controller.js
+++ public/js/controllers/register.controller.js
@@ -5,5 +5,25 @@ RegisterController.$inject = ['$scope', '$location', 'AuthService'];
 
 function RegisterController($scope, $location, AuthService) {
 
-    
+  $scope.register = function(){
+
+    // initial
+    $scope.error = false;
+    $scope.disabled = false;
+
+    //
+    AuthService.register($scope.registerForm.username,
+       $scope.registerForm.password)
+          .then(function(response){
+            $location.path('/login');
+            $scope.disabled = false;
+            $scope.registrstionForm = {};
+          })
+          .catch(function(err){
+            $scope.error = true;
+            $scope.errorMessage = 'Somthing went wrong';
+            $scope.disabled = false;
+            $scope.registerForm = {};
+          });
+  }
 };
--- public/js/services/auth.service.js
+++ public/js/services/auth.service.js
@@ -4,7 +4,82 @@ angular.module('myApp')
 AuthService.$inject = ['$q', '$timeout', '$http'];
 
 function AuthService($q, $timeout, $http) {
+  var user = null;
 
-    
+    return{
+      isLoggedIn: isLoggedIn,
+      getUserStatus: getUserStatus,
+      login,
+      logout: logout,
+      register: register,
+    };
 
-};
+    function isLoggedIn(){}
+      if(user){
+        return  true;
+      }else {
+        return false;
+      }
+
+    function getUserStatus(){
+      $http.get('/user/status')
+        .then(function(response){
+          if(response.data.status){
+            user = true;
+          }else {
+            user = false;
+          }
+        })
+          .catch(function(err){
+            user = false;
+        });
+      }
+    function login(username, password){
+      var defered = $q.defer();
+      $http.post('/user,/login',{username: username, password: password,})
+        .then(function(response){
+          if(response.data.status){
+            user = true;
+            deferred.resolve();
+          } else {
+            user = false;
+            deferred.reject();
+          }
+        })
+        .catch(function(err){
+          user = false;
+          deffered.reject();
+        });
+        return deferred.promise;
+    }
+
+    function logout(){
+      $http.get('/user/logout')
+        .then(function(response){
+          user = false;
+          deferred.resolve();
+        })
+        .catch(function(response){
+          user = false;
+          deferred.reject();
+        });
+        return deferred.promise;
+      }
+      function register(username, password){
+          var deffered = $q.defer();
+          $http.post('/user/register',
+        {username: username, password: password})
+        .then(function(response){
+          if(response.data.status){
+            deferred.resolve()
+            }else{
+              deferred.reject();
+          }
+        })
+        .catch(function(err){
+          deferred.reject();
+        });
+
+        return deferred.promise;
+        }
+      };
